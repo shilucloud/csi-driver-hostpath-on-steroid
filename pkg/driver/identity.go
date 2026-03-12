5d94a43594f50eeb9ad360cc1c79c6c943b6ee96
shilucloud <shilash112001@gmail.com>
2026-03-12 11:50:39 +0530

    update: identity and node function skeleton

--- pkg/driver/identity.go
+++ pkg/driver/identity.go
@@ -1,4 +1,26 @@
 package driver
 
+import (
+	"fmt"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+)
+
 type IdentityService struct {
+}
+
+func (is *IdentityService) GetPluginCapabilities(*csi.GetPluginCapabilitiesRequest) *csi.GetPluginCapabilitiesResponse {
+	fmt.Print("This is plugin cap")
+
+	return nil
+}
+
+func (is *IdentityService) GetPluginInfo(*csi.GetPluginInfoRequest) *csi.GetPluginInfoResponse {
+	fmt.Print("this is plugin info")
+	return nil
+}
+
+func (is *IdentityService) Probe(*csi.ProbeRequest) *csi.ProbeResponse {
+	fmt.Print("this is probe")
+	return nil
 }
--- pkg/driver/node.go
+++ pkg/driver/node.go
@@ -1,4 +1,34 @@
 package driver
 
+import (
+	"fmt"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+)
+
 type NodeService struct {
+}
+
+func (ns *NodeService) NodeStageVolume(*csi.NodeStageVolumeRequest) *csi.NodeStageVolumeResponse {
+	fmt.Print("this is nodestage vol")
+
+	return nil
+}
+
+func (ns *NodeService) NodeUnStageVolume(*csi.NodeUnstageVolumeResponse) *csi.NodeUnstageVolumeResponse {
+	fmt.Print("this is nodeUnstage vol")
+
+	return nil
+}
+
+func (ns *NodeService) NodePublishVolume(*csi.NodePublishVolumeRequest) *csi.NodePublishVolumeResponse {
+	fmt.Print("this iss nodepubvol")
+
+	return nil
+}
+
+func (ns *NodeService) NodeUnpublishVolume(*csi.NodeUnpublishVolumeRequest) *csi.NodeUnpublishVolumeResponse {
+	fmt.Print("this iss nodeunpubvol")
+
+	return nil
 }
